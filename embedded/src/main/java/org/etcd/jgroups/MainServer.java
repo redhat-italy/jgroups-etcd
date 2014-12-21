dde7577e13f4d427260bebfffc44d4c013390561
ugol <ugol@redhat.com>
2014-12-21 18:46:43 +0100

    Added TJWS Embedded Server plus example Deploy

--- embedded/src/main/java/org/etcd/jgroups/MainServer.java
+++ embedded/src/main/java/org/etcd/jgroups/MainServer.java
@@ -1,12 +1,24 @@
 package org.etcd.jgroups;
 
+import org.jboss.resteasy.plugins.server.tjws.TJWSEmbeddedJaxrsServer;
+import org.jboss.resteasy.spi.ResteasyDeployment;
+import org.jboss.resteasy.test.TestPortProvider;
+import org.jgroups.etcd.rest.EtcdJaxRS;
+
 public class MainServer {
 
     public static void main(String[] args) {
 
         System.out.println("Starting Embedded Server");
-
-
+        TJWSEmbeddedJaxrsServer tjws = new TJWSEmbeddedJaxrsServer();
+        ResteasyDeployment deployment = new ResteasyDeployment();
+        deployment.setApplication(new EtcdJaxRS());
+        tjws.setDeployment(deployment);
+        tjws.setBindAddress(TestPortProvider.getHost());
+        tjws.setPort(TestPortProvider.getPort());
+        tjws.setRootResourcePath("");
+        tjws.setSecurityDomain(null);
+        tjws.start();
     }
 
 }
